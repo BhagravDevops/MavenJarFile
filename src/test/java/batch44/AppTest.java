bf10069732c5c1253906f41c32fb55dacb74722c
unknown <bhargav522@gmail.com>
2023-04-30 19:50:31 +0530

    V5

---
+++ src/test/java/batch44/AppTest.java
@@ -0,0 +1,20 @@
+package batch44;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
